182f4b1dcccf958f7593118c3c8f54b0dc4879ff
dmod129 <derek.modugno@gmail.com>
2023-08-20 18:19:30 -0500

    added variables and prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,28 @@
 // Assignment code here
 
+// Variables
+var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
+var numbericalChar = "0123456789";
+// var specialChar = " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+// Generate password
+
+function generatePassword() {
+  console.log("Button clicked");
+  let passwordLength = prompt("Please enter desired password length");
+    if (isNaN(passwordLength)) {
+      return ("That is not a number.");
+    
+}
+// Prompt for password criteria
+  // Length 8 - 128
+  // Lowercase, uppercase, numbers, special characters
+// Validate input
+// Generate password
+
+// Display password to page
+  return ("Here's the password!");
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
