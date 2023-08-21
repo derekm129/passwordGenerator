a2c8cd71f7301c1e1be8a378814106659a17a038
dmod129 <derek.modugno@gmail.com>
2023-08-20 22:01:51 -0500

    created function checkNumbers

--- Develop/script.js
+++ Develop/script.js
@@ -3,27 +3,68 @@
 // Variables
 var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
-var numbericalChar = "0123456789";
+var numericalChar = "0123456789";
 // var specialChar = " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-// Generate password
 
+// Generate password
 function generatePassword() {
-  console.log("Button clicked");
-  let passwordLength = prompt("Please enter desired password length");
+  let check = checkLength();
+  if (checkLength()) {
+    prompt("Would you like numbers in your password?");
+  }
+  if (checkNumbers()) {
+    prompt("Would you like upper case letters?");
+  }
+}  
+  
+  // let numbers = prompt("would you like numbers?")
+function checkNumbers() {
+  const response = confirm("Would you like numbers?"); 
+    if (response) {
+      console.log("Your password will have numbers.");
+    } else {
+      console.log("Your password will not have numbers.")
+    }
+  }
+  
+
+  // let lowerCase = prompt("Would you like lower case letters?")
+function lowerCase() {
+  const response = confirm("Would you like lowercase numbers.")
+}
+  // let upperCase = prompt("Would you like upper case letters?")
+
+  // let special = prompt("Would you like special characters?")
+ 
+  
+
+ 
+
+
+// Check password length
+function checkLength() {
+  let passwordLength = prompt("Please enter password length between 8 and 128 characters.");
     if (isNaN(passwordLength)) {
-      return ("That is not a number.");
-    
-}
-// Prompt for password criteria
-  // Length 8 - 128
-  // Lowercase, uppercase, numbers, special characters
-// Validate input
-// Generate password
-
-// Display password to page
-  return ("Here's the password!");
+      console.log("That is not a number.");
+      return false;
+  } else if (passwordLength < 8 || passwordLength > 128) {
+      console.log("Password must be between 8 and 128 characters.");
+      return false;
+    } return true;
 }
 
+
+if (checkLength()) {
+  let numbers = checkNumber();
+  let lowers = checkLower();
+
+//at the end you're going to use these to create the final return
+  // return password;
+}
+
+
+
+// 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
